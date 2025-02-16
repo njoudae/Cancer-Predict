d622960ca24a9ebe6c7f090b0ab40ea99be4a514
njoudae <njoodeshaq@gmail.com>
2025-02-16 16:44:14 +0300

    qw

--- app/main.py
+++ app/main.py
@@ -163,7 +163,7 @@ def add_predictions(input_data):
     input_array_scaled = scaler.transform(input_array)
     prediction = model.predict(input_array_scaled) # 0=b | 1=m
 
-    st.subheader("Cell Cluster Prediction: ")
+    st.header("Cell Cluster Prediction: ")
     if prediction[0] == 0:
         st.write("<span class='diagnosis be'>Benign</span>", unsafe_allow_html=True)
     else:
